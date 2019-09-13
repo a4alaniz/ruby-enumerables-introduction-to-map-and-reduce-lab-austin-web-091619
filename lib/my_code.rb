03ec06a65187ad5ef35b871ad48865a6e27cb68a
Angel Alanis <a4alaniz@gmail.com>
2019-09-13 22:18:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -34,11 +34,16 @@ new_array
 end 
 
 def map_to_square(array)
-  array = [1, 2, 3, -9]
-  array.map{|n| n**2}
+  new_array = []
+  i = 0 
+  while i < array.length
+  new_array.push(array[i]**2)
+  i += 1
+end
+new_array
 end  
   
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point=0)
   source_array = [1,2,3]
   source_array.reduce{|source_array| sum += source_array}
 end  
