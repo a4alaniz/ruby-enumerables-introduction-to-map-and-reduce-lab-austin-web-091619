f8d05c772a4ced8d901436165b09915cf77c2311
Angel Alanis <a4alaniz@gmail.com>
2019-09-13 21:57:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,37 @@
-# My Code here....
+def map_to_negativize(array) 
+  array = [1, 2, 3, -9]
+  array.map{|n| n * -1}
+end
+
+def map_to_no_change(dune)
+  dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
+  dune.map{|n| n}
+end  
+
+def map_to_double(array)
+  array = [1, 2, 3, -9]
+  array.map{|n| n * 2}
+end 
+
+def map_to_square(array)
+  array = [1, 2, 3, -9]
+  array.map{|n| n**2}
+end  
+  
+def reduce_to_total(source_array, starting_point)
+  source_array = [1,2,3]
+  source_array.reduce{|source_array| sum += source_array}
+end  
+  
+def reduce_to_total(source_array, starting_point)
+  source_array = [1,2,3]
+  source_array.reduce(100) {|sum, starting_point| sum + starting_point}
+end
+  
+def reduce_to_all_true(source_array) 
+  source_array.reduce { |beTru, beFalse| beTru && beFalse}
+end
+
+def reduce_to_any_true(source_array) 
+  source_array.reduce { |beTru, beFalse| beTru || beFalse}
+end
